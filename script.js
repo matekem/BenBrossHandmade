b6470b79f857a3f920fe42c78c82b7c7ec88fed3
Máté Kemenesi <mate.kemenesi@gmail.com>
2023-01-22 16:05:53 +0100

    Finished polishing/animating landing page

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const navList = document.getElementById("navList")
 const topMenuLine = document.getElementById("menuTop")
 const midMenuLine = document.getElementById("menuMid")
 const bottomMenuLine = document.getElementById("menuBottom")
+const navBar = document.getElementById("navBar")
 
 toggleBtn.addEventListener("click", () =>{
    
@@ -10,6 +11,7 @@ toggleBtn.addEventListener("click", () =>{
     topMenuLine.classList.toggle("top-active")
     midMenuLine.classList.toggle("mid-active")
     bottomMenuLine.classList.toggle("bottom-active")
+    navBar.classList.toggle("navbar-active")
 })
 
 
