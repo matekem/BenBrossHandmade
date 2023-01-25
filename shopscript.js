45643b8bcd8c4d3dd13d1050486b97c9a2b663b7
matekem <97744782+matekem@users.noreply.github.com>
2023-01-25 16:10:40 +0100

    Create shopscript.js

---
+++ shopscript.js
@@ -0,0 +1,16 @@
+const itemShop = document.getElementById("itemShop");
+const imageContainer = document.getElementById("imgContainer")
+let imageArray = []
+function fillImageArray(imageArray){
+    imageArray.push("Images/img1.jpg")
+    imageArray.push("Images/img2.jpg")
+    imageArray.push("Images/img3.jpg")
+    //imageArray.push("Images/img4.jpg")
+    imageArray.push("Images/img5.jpg")
+    imageArray.push("Images/img6.jpg")
+    imageArray.push("Images/img7.jpg")
+
+    return imageArray
+}
+    fillImageArray(imageArray)
+
