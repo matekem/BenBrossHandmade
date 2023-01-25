7ea9dd7772ca3345235d7eff4bbdcb8c0fd95263
Máté Kemenesi <mate.kemenesi@gmail.com>
2023-01-25 18:51:47 +0100

    bugfixing

--- JS/shopscript.js
+++ JS/shopscript.js
@@ -1,5 +1,20 @@
 const itemShop = document.getElementById("itemShop");
 const imageContainer = document.getElementById("imgContainer")
+const toggleBtn = document.getElementById("toggleBtn")
+const navList = document.getElementById("navList")
+const topMenuLine = document.getElementById("menuTop")
+const midMenuLine = document.getElementById("menuMid")
+const bottomMenuLine = document.getElementById("menuBottom")
+const navBar = document.getElementById("navBar")
+
+toggleBtn.addEventListener("click", () =>{
+   
+    navList.classList.toggle("active")
+    topMenuLine.classList.toggle("top-active")
+    midMenuLine.classList.toggle("mid-active")
+    bottomMenuLine.classList.toggle("bottom-active")
+    navBar.classList.toggle("navbar-active")
+})
 let imageArray = []
 function fillImageArray(imageArray){
     imageArray.push("Images/img1.jpg")
