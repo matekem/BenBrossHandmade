b150cf61462ca2cf08f0fab81180ee957a9c1e5c
matekem <97744782+matekem@users.noreply.github.com>
2023-01-26 07:40:49 +0000

    Added new files

---
+++ JS/shopcategories.js
@@ -0,0 +1,15 @@
+const toggleBtn = document.getElementById("toggleBtn")
+const navList = document.getElementById("navList")
+const topMenuLine = document.getElementById("menuTop")
+const midMenuLine = document.getElementById("menuMid")
+const bottomMenuLine = document.getElementById("menuBottom")
+const navBar = document.getElementById("navBar")
+
+toggleBtn.addEventListener("click", () =>{
+   
+    navList.classList.toggle("active")
+    topMenuLine.classList.toggle("top-active")
+    midMenuLine.classList.toggle("mid-active")
+    bottomMenuLine.classList.toggle("bottom-active")
+    navBar.classList.toggle("navbar-active")
+})
